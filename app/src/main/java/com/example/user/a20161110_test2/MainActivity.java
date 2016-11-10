c498bc9e72a1cc532f27196ae8fc3f3a28f8173f
lili <liq1862@gmail.com>
2016-11-10 13:49:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/a20161110_test2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.user.a20161110_test2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("關於我們");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getTitle().toString().equals("關於我們"))
+        {
+            Toast.makeText(MainActivity.this,"按下關於我們",Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
